e666b8baea72b90d33010da1270039865d039afd
Evagehring <evamaria.gehring@student.hnu.de>
2025-06-02 15:44:21 +0200

    neu?

--- src/main/java/BestellManager/BestellManager.java
+++ src/main/java/BestellManager/BestellManager.java
@@ -1,6 +1,36 @@
 package BestellManager;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BestellManager extends JFrame {
-}
+
+    private JComboBox KaffeArt;
+    private JRadioButton jatogo;
+    private JRadioButton neintogo;
+    private JButton berechne;
+    private JComboBox MilchSorte;
+    private JTextField tfPreis;
+    private JPanel myPanel;
+
+    public BestellManager() {
+       setVisible(true);
+       setSize(500, 500);
+       setContentPane(myPanel);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        berechne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+tfPreis.setText("preis");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new BestellManager();
+    }
+    }
+
--- src/main/java/BestellManager/Kaffee.java
+++ src/main/java/BestellManager/Kaffee.java
@@ -1,8 +1,59 @@
 package BestellManager;
 
 public class Kaffee {
-    String kaffeeArt;
-    boolean istIced;
-    String milch;
-    double preis;
+    private String kaffeeArt;
+    private String milch;
+    private boolean togo;
+    private int anzahl;
+    private double preis;
+
+    public Kaffee(String kaffeeArt, boolean togo, String milch, int anzahl) {
+        this.kaffeeArt = kaffeeArt;
+        this.milch = milch;
+        this.togo = togo;
+        this.anzahl = anzahl;
+        this.preis = berechnePreis();
+
+    }
+
+    private double berechnePreis() {
+        double grundpreis;
+
+        switch (kaffeeArt) {
+            case "Espresso":
+                grundpreis = 2.20;
+                break;
+
+            case "Americano":
+                grundpreis = 2.70;
+                break;
+
+            case "Cappuccino":
+                grundpreis = 3.30;
+                break;
+
+            case "Iced Latte":
+                grundpreis = 4.20;
+                break;
+
+            default:
+                System.out.println("Getränk nicht gefunden");
+                grundpreis = 0.0;
+
+        }
+        if (milch.equals("Hafermilch") || milch.equals("Sojamilch")) {
+            preis = grundpreis + 0.30;
+        }
+        if (togo) {
+            preis = grundpreis + 0.50;
+        }
+
+        return grundpreis;
+    }
+    public double getPreis() {
+        return preis;
+    }
+    public String toString() {
+    return kaffeeArt;
+    }
 }
