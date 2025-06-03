684e8730901e8c30ee41722a4f0f18734587a1c8
Evagehring <evamaria.gehring@student.hnu.de>
2025-06-03 13:19:24 +0200

    03.06.

--- src/main/java/BestellManager/BestellManager.java
+++ src/main/java/BestellManager/BestellManager.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 public class BestellManager extends JFrame {
 
+    private Kaffee kaffee;
     private JComboBox KaffeArt;
     private JRadioButton jatogo;
     private JRadioButton neintogo;
@@ -13,6 +14,7 @@ public class BestellManager extends JFrame {
     private JComboBox MilchSorte;
     private JTextField tfPreis;
     private JPanel myPanel;
+    private JTextField tfAnzahl;
 
     public BestellManager() {
        setVisible(true);
@@ -20,17 +22,43 @@ public class BestellManager extends JFrame {
        setContentPane(myPanel);
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+JComboBox<String> kaffeeArtBox = new JComboBox<>(new String[] {
+    "Espresso", "Americano", "Cappuccino", "Iced Latte"
+});
+        JComboBox<String> milchBox = new JComboBox<>(new String[] {
+                "Kuhmilch", "Hafermilch", "Sojamilch"
+        });
 
-        berechne.addActionListener(new ActionListener() {
+        JTextField tfAnzahl = new JTextField();
+
+        JRadioButton jatogo = new JRadioButton("true");
+        JRadioButton neintogo = new JRadioButton("false");
+
+        ButtonGroup togoButtons = new ButtonGroup();
+        togoButtons.add(jatogo);
+        togoButtons.add(neintogo);
+
+        KaffeArt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-tfPreis.setText("preis");
+                String gewaehlteArt = KaffeArt.getSelectedItem().toString();
+                String gewaehlteMilch = MilchSorte.getSelectedItem().toString();
+                boolean togo = jatogo.isSelected();
+
+                int anzahl = 1;
+
+
+                Kaffee kaffee1 = new Kaffee(gewaehlteArt, togo, gewaehlteMilch, anzahl);
+                double preis = kaffee1.getPreis();
+                tfPreis.setText(String.format("%.2f €", kaffee1.getPreis()));
             }
         });
-    }
 
-    public static void main(String[] args) {
-        new BestellManager();
+            }
+
+            public static void main(String[] args) {
+                new BestellManager();
+
     }
-    }
+}
 
